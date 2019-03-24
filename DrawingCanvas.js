395fb319da2083a3dfb11d408447218d0600bd16
Edda Pétursdóttir <eddapetursdottir@gmail.com>
2019-03-24 20:32:04 +0000

    Loads of changes in ui

--- DrawingCanvas.js
+++ DrawingCanvas.js
@@ -4,64 +4,102 @@
 document.addEventListener("DOMContentLoaded", init, false);
 
 var drawing = false;
-var canvas;
-var context;
+var heightCanvas;
+var colorCanvas;
+var hmContext;
+var cmContext;
 var color;
 
-// Array for X and Y coordinate
-var clickX = [];
-var clickY = [];
-var weight = [];    // Pencil weight
-var stroke = [];    // Opacity/color
-// var colorArr = [];
-var clickDrag = [];
+// Arrays for heightmap properties
+var maps = {
+    'heightmap': {
+        X: [],
+        Y: [],
+        weight: [],
+        stroke: [],
+        clickDrag: [],
+        context: ''
+    },
+    'colormap': {
+        X: [],
+        Y: [],
+        weight: [],
+        stroke: [],
+        clickDrag: [],
+        context: ''
+    }
+};
+
 
 // Sliders
 var weightSlider;
 var opacitySlider;
+var weightSlider2;  // Color canvas
 
 
 function init()
 {
-    testfunction("Edda");
-    // Mouse movements
-    canvas = document.getElementById("heightmap");
+    // Get canvas for height
+    heightCanvas = document.getElementById("heightmap");
+    maps.heightmap.context = heightCanvas.getContext("2d");
+    setEventListeners(heightCanvas);
+    weightSlider = document.getElementById("weightSlider");
+    setWeightSlider(maps.heightmap.context, weightSlider);
+    setOpacitySlider(maps.heightmap.context);
+
+    colorCanvas = document.getElementById("colormap");
+    maps.colormap.context = colorCanvas.getContext("2d");
+    setEventListeners(colorCanvas);
+    weightSlider2 = document.getElementById("weightSlider2");
+    setWeightSlider(maps.colormap.context, weightSlider);
+
+
+    hmContext = maps.heightmap.context;
+    hmContext.fillStyle = "black";
+    hmContext.fillRect(0, 0, hmContext.canvas.width, hmContext.canvas.height);
+
+
+    // Clear buttons
+    document.getElementById("clearHeightmap").onclick = clearMap('heightmap');
+    document.getElementById("clearColormap").onclick = clearMap('colormap');
+
+
+
+    color = opacitySlider.value * 2.55;
+    setHeightStrokeStyle(color);
+}
+
+
+function setEventListeners(canvas){
     canvas.addEventListener("mousedown", startDrawing, false);
     canvas.addEventListener("mouseup", stopDrawing, false);
     canvas.addEventListener("mousemove", draw, false);
-    // canvas.addEventListener("mouseleave", stopDrawing, false);
+}
 
-    context = canvas.getContext("2d");
-    context.fillStyle = "black";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-    document.getElementById("clearButton").onclick = clear;
-
-    // WeightSlider
-    weightSlider = document.getElementById("weightSlider");
-    context.lineWidth = weightSlider.value;                         // Size Initialize value as weight slider
-    // context.lineWidth = 400;
-    weightSlider.oninput = function(){
-        console.log('slider', this.value);
-        context.lineWidth = this.value;                         // Size Initialize value as weight slider
+function setWeightSlider(ctx, slider){
+    ctx.lineWidth = slider.value;                         // Size Initialize value as weight slider
+    slider.oninput = function(){
+        ctx.lineWidth = this.value
     };
+}
 
-    // Opacity
+function setOpacitySlider(ctx){
     opacitySlider = document.getElementById("opacitySlider");
     opacitySlider.oninput = function(){
-        console.log('slider', this.value);
+        // console.log('slider', this.value);
         color = this.value * 2.55;
-        context.strokeStyle = 'rgb(' + color + ','  + color + ', '  + color + ', ' + 0.1 + ')';
-        context.shadowColor = 'rgb(' + color + ','  + color + ', '  + color + ')';
+        ctx.strokeStyle = 'rgb(' + color + ','  + color + ', '  + color + ', ' + 0.1 + ')';
+        ctx.shadowColor = 'rgb(' + color + ','  + color + ', '  + color + ')';
     };
+}
 
-    color = opacitySlider.value * 2.55;
-    context.strokeStyle = 'rgb(' + color + ','  + color + ', '  + color + ', ' + 0.1 + ')';
-    context.lineWidth = weightSlider.value;                         // Size Initialize value as weight slider
-    context.lineJoin = "round";             // Style
+function setHeightStrokeStyle(color){
+    hmContext.strokeStyle = 'rgb(' + color + ','  + color + ', '  + color + ', ' + 0.1 + ')';
+    hmContext.lineWidth = weightSlider.value;                         // Size Initialize value as weight slider
+    hmContext.lineJoin = "round";             // Style
     // context.shadowColor = 'rgb(255, 255, 255)';
-    context.shadowColor = 'rgb(' + color + ','  + color + ', '  + color + ')';
-    context.shadowBlur  = 100;
+    hmContext.shadowColor = 'rgb(' + color + ','  + color + ', '  + color + ')';
+    hmContext.shadowBlur  = 100;
 }
 
 
@@ -69,7 +107,6 @@ function startDrawing(e){
     console.log("draw!");
     drawing = true;
     draw(e);
-
 }
 
 
@@ -78,12 +115,14 @@ function stopDrawing(e){
 }
 
 function draw(e){
+    console.log("Draw: ", e.target.id);
+    var map = e.target.id;
     if(drawing){
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
 
-        addClick(mouseX, mouseY, true);
-        redraw();
+        addClick(mouseX, mouseY, map, true);
+        redraw(map);
         // if(drawing){
         //     addSingle(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
         //     // context.stroke();
@@ -93,40 +132,29 @@ function draw(e){
 }
 
 
-function addClick(x, y, dragging)
+function addClick(x, y, map, dragging)
 {
-
-    // var grd = context.createRadialGradient(x, y, 4, 90, 60, 100);
-    // grd.addColorStop(0, "red");
-    // grd.addColorStop(1, "white");
-    // context.strokeStyle = grd;
-
-    // var maxWidth = context.lineWidth;
-    clickX.push(x);
-    clickY.push(y);
-    weight.push(context.lineWidth);
-    stroke.push(context.strokeStyle);
-    clickDrag.push(dragging);
-    // context.lineTo(x, y);
-    // var x0 = (x - 10);
-    // var y0 = (y - 10);
-    // var x1 = (x + 10);
-    // var y1 = (y + 10);
-
-    // var grd = context.createRadialGradient(x0, y0, 0, x1, y1, 30);
-    // grd.addColorStop(0, "red");
-    // grd.addColorStop(1, "green");
-    // context.strokeStyle = grd;
-    // stroke.push(grd);
+    console.log('Map: ', map);
+    maps[map].X.push(x);
+    maps[map].Y.push(y);
+    // map.weight.push(map.context.lineWidth);
+    // map.stroke.push(map.context.strokeStyle);
+    // map.clickDrag.push(dragging);
 }
 
 
-function redraw(){
+function redraw(map){
+    var context = maps[map].context;
+    var weight = maps[map].weight;
+    var stroke = maps[map].stroke;
+    var clickX = maps[map].X;
+    var clickY =  maps[map].Y;
+    var clickDrag = maps[map].clickDrag;
+
+
     context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
     context.fillStyle = "black";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    console.log('Redrawing!');
-
 
     for(var i = 0; i < clickX.length; i++) {
         context.lineWidth = weight[i];
@@ -141,26 +169,28 @@ function redraw(){
         context.closePath();
         context.stroke();
     }
-    generate();
+    generate(map);
 }
 
-function clear(){
-    console.log('Clearing!');
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-    context.fillStyle = "black";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    // opacitySlider.value = 50;
-    clickX = [];
-    clickY = [];
-    clickDrag = [];
-    generate();
+function clearMap(map){
+    var ctx = maps[map].context;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    maps[map].X = [];           // X coordinates
+    maps[map].Y = [];           // Y coordinates
+    maps[map].lickDrag = [];
+    generate(map);
 }
 
-function generate(){
-    var imageData = context.getImageData(0, 0, 320, 320);
-    console.log(imageData);
+function generate(map){
+    var imageData = maps[map].context.getImageData(0, 0, 320, 320);
     var array = new Uint8Array(imageData.data.buffer);
-    setHeightmap(array);
+    if(map == 'heightmap'){
+        setHeightmap(array);
+    } else if (map == 'colormap'){
+
+    }
 }
 
 
